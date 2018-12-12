0179e0cbc2ec4a8c265bacbbb2f37ec7b34b5d09
Pedro Henrique <pedrohenrique@Pedros-MacBook-Pro.local>
2018-12-12 19:13:48 -0200

    Correcao detalhe post

--- src/Components/DetalhePost.js
+++ src/Components/DetalhePost.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
+import { Segment, Dimmer, Loader, Label } from "semantic-ui-react";
+
+import Post from "./Post";
 
 class DetalhePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: null
+    };
+  }
+
+  componentDidMount = () => {
+    const posts = JSON.parse(localStorage.getItem("savedPosts"));
+    const post = posts
+      .filter(savedPost => {
+        return savedPost.time === this.props.match.params.time;
+      })
+      .pop();
+
+    this.setState({ post });
+  };
+
   render() {
-    return (
-      <div>
-        <text>oi</text>
-      </div>
-    );
+    if (this.state.post === null) {
+      return (
+        <Segment
+          style={{
+            marginTop: window.innerHeight * 0.5,
+            height: 150
+          }}
+        >
+          <Dimmer active>
+            <Loader>Carregando</Loader>
+          </Dimmer>
+        </Segment>
+      );
+    } else {
+      return (
+        <div>
+          <Post post={this.state.post} />
+        </div>
+      );
+    }
   }
 }
 
